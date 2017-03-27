9dd29fda07e1863d3e7a89d87e313230c5095d0e
jtran10 <tranj10@gmail.com>
2017-03-27 18:17:34 -0500

    call get_xgb_test_auc function, bayesian optimization

--- scripts/main_xgboost.R
+++ scripts/main_xgboost.R
@@ -1,14 +1,19 @@
-## main file to run entire program
+## main file to run program
 # github.com/jtran10/sds385_kaggle2
+
+## notes:
+# - program needs to run all at once to ensure reproducability by seed
+# 
 
 ## Clear workspace
 rm(list=ls())
 
 ## load libraries
 library(xgboost)
-library(Matrix)
 library(caTools)
 library(caret)
+library(rBayesianOptimization)
+library(parallel)
 
 ## set working directory
 this.dir = dirname(parent.frame(2)$ofile)
@@ -26,7 +31,7 @@ modelTrainData = train9698Data
 modelTestData = test9698Data
 
 ## create k-fold lists (test indices)
-kFolds = 10
+kFolds = 10L
 set.seed(myseed)
 testFoldInd = createFolds(modelTrainData$y, kFolds, returnTrain = FALSE)
 set.seed(myseed)
@@ -45,9 +50,10 @@ bounds = list(max_depth = c(1L, 20L),
 
 ## misc inputs to xgboost
 init_early = 3L
+nthread = detectCores() - 1
 
 ## create random parameter search grid
-randLen = 4 # numer of search points in random grid
+randLen = 4L # number of search points in random grid
 source('rand_search_grid.R')
 set.seed(myseed)
 random_grid = create_random_grid(randLen, bounds)
@@ -58,7 +64,36 @@ xgbTestData = model.matrix(~ ., data = modelTestData)[,-1]
 train.y = ifelse(modelTrainData$y == "y", 1, 0)
 dtrain = xgb.DMatrix(xgbTrainData, label = train.y)
 
-##
+## get all kfold test AUC for random grid
+source('post_model_feature_eng.R')
+source('get_xgb_testAUC.R')
+randGridAUC = matrix(NA,randLen,1L)
+for (i in 1:randLen){
+  randGridAUC[i,1] = get_xgb_testAUC(random_grid$max_depth[i],
+                                     random_grid$gamma[i],
+                                     random_grid$colsample_bytree[i],
+                                     random_grid$min_child_weight[i],
+                                     random_grid$subsample[i],
+                                     random_grid$scale_pos_weight[i])[[1]]
+}
+
+## summarize random search results into data frame
+initial_bayes_grid = data.frame(random_grid, Value = randGridAUC)
+
+## bayesian optimization of tuning parameters
+bayesLen = 4L # number of search points for bayesian optimization
+bayes_result = BayesianOptimization(
+                  get_xgb_testAUC,
+                  bounds = bounds,
+                  init_grid_dt = initial_bayes_grid,
+                  init_points = 0,
+                  n_iter = 4,
+                  acq = "ucb",
+                  kappa = 4.0,
+                  eps = 0.0,
+                  verbose = TRUE
+)
+
 
 
 
