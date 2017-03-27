718adb67e580bd1ccc3f5a7729f5622a2c09f42a
jtran10 <tranj10@gmail.com>
2017-03-27 15:10:24 -0500

    parameter bounds and random search grid

--- scripts/main_xgboost.R
+++ scripts/main_xgboost.R
@@ -1,4 +1,5 @@
 ## main file to run entire program
+# github.com/jtran10/sds385_kaggle2
 
 ## Clear workspace
 rm(list=ls())
@@ -6,7 +7,8 @@ rm(list=ls())
 ## load libraries
 library(xgboost)
 library(Matrix)
-library(pROC)
+library(caTools)
+library(caret)
 
 ## set working directory
 this.dir = dirname(parent.frame(2)$ofile)
@@ -16,7 +18,34 @@ setwd(this.dir)
 source('data_loading_feature_engineering.R')
 
 ## set seed
-myseed = 555
+myseed = 555L
 set.seed(myseed)
 
-## 
+# select data set to use
+modelTrainData = train9698Data
+
+## create k-fold lists (test indices)
+kFolds = 10
+testFoldInd = createFolds(modelTrainData$y, kFolds, returnTrain = FALSE)
+
+## input initial constant tuning params and bounds
+init_nrounds = 50000L # basically infinite
+init_eta = .01 
+bounds = list(max_depth = c(1L, 20L),
+              gamma = c(0.0,20.0),
+              colsample_bytree = c(0.1, 1.0),
+              min_child_weight = c(0L, 60L),
+              subsample = c(0.1, 1.0),
+              scale_pos_weight = c(0.1,8.0)
+              )
+
+## misc inputs to xgboost
+init_early = 100L
+
+## create random parameter search grid
+randLen = 4 # numer of search points in random grid
+source('rand_search_grid.R')
+random_grid = create_random_grid(randLen, bounds)
+
+
+
---
+++ scripts/rand_search_grid.R
@@ -0,0 +1,17 @@
+## function to create return random search grid
+create_random_grid <- function(randLen, bounds ){
+random_grid = data.frame(nrounds = matrix(init_nrounds,randLen),
+  eta = matrix(init_eta,randLen),
+  max_depth = sample(bounds$max_depth[1]:bounds$max_depth[2], 
+                     replace = TRUE, size = randLen),
+  gamma = runif(randLen, min = bounds$gamma[1], max = bounds$gamma[2]),
+  colsample_bytree = runif(randLen, min = bounds$colsample_bytree[1], 
+                           max = bounds$colsample_bytree[2]),
+  min_child_weight = sample(bounds$min_child_weight[1]:bounds$min_child_weight[2], 
+                            replace = TRUE, size = randLen),
+  subsample = runif(randLen, min = bounds$subsample[1], max = bounds$subsample[2]),
+  scale_pos_weight = runif(randLen, min = bounds$scale_pos_weight[1], 
+                           max = bounds$scale_pos_weight[2])
+  )
+return(random_grid)
+}
