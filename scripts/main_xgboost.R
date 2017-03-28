72fbfc990e4fe46817b6a32fd62607abc3f05a75
jtran10 <tranj10@gmail.com>
2017-03-28 23:49:09 +0000

    latest

--- scripts/main_xgboost.R
+++ scripts/main_xgboost.R
@@ -2,8 +2,11 @@
 # github.com/jtran10/sds385_kaggle2
 
 ## notes:
+# - see readme for program process description
 # - program needs to run all at once to ensure reproducability by seed
-# 
+# - init_eta, init_early, randLen, bayesLen for kaggle solution:
+#      0.05, 75L, 150L, 50L
+# - the above parameters are modified here for reasonable run time on laptop
 
 ## Clear workspace
 rm(list=ls())
@@ -14,6 +17,7 @@ library(caTools)
 library(caret)
 library(rBayesianOptimization)
 library(parallel)
+library(tictoc)
 
 ## set working directory
 this.dir = dirname(parent.frame(2)$ofile)
@@ -39,22 +43,21 @@ trainFoldInd = createFolds(modelTrainData$y, kFolds, returnTrain = TRUE)
 
 ## input initial constant tuning params and bounds
 init_nrounds = 50000L # basically infinite
-init_eta = .01 
+init_eta = .05 
 bounds = list(max_depth = c(1L, 20L),
               gamma = c(0.0,20.0),
               colsample_bytree = c(0.1, 1.0),
               min_child_weight = c(0L, 60L),
               subsample = c(0.1, 1.0),
-              scale_pos_weight = c(0.1,8.0)
+              scale_pos_weight = c(0.1,10.0)
               )
 
 ## misc inputs to xgboost
-init_early = 3L
+init_early = 75L
 nthread = detectCores()
 
-
 ## create random parameter search grid
-randLen = 4L # number of search points in random grid
+randLen = 150L # number of search points in random grid
 source('rand_search_grid.R')
 set.seed(myseed)
 random_grid = create_random_grid(randLen, bounds)
@@ -71,33 +74,43 @@ source('get_xgb_testAUC.R')
 randGridAUC = matrix(NA,randLen,1L)
 for (i in 1:randLen){
   print(c("randIter",i))
+  tic()
   randGridAUC[i,1] = get_xgb_testAUC(random_grid$max_depth[i],
                                      random_grid$gamma[i],
                                      random_grid$colsample_bytree[i],
                                      random_grid$min_child_weight[i],
                                      random_grid$subsample[i],
                                      random_grid$scale_pos_weight[i])[[1]]
+  toc()
 }
 
 ## summarize random search results into data frame
-initial_bayes_grid = data.frame(random_grid, Value = randGridAUC)
+random_grid_summary = data.frame(random_grid, Value = randGridAUC)
+# use best N results as initial points for bayesian optimization
+Nbest = 50L
+ordered_by_value_rand_summary = 
+  random_grid_summary[order(random_grid_summary$Value, decreasing = TRUE),]
+rownames(ordered_by_value_rand_summary) <- NULL
+initial_bayes_grid = ordered_by_value_rand_summary[1:Nbest,]
+
+
+save.image(file="post_rand_search.RData")
 
 ## bayesian optimization of tuning parameters
-bayesLen = 4L # number of search points for bayesian optimization
+bayesLen = 25L # number of search points for bayesian optimization
+set.seed(myseed)
 bayes_result = BayesianOptimization(
                   get_xgb_testAUC,
                   bounds = bounds,
                   init_grid_dt = initial_bayes_grid,
                   init_points = 0,
-                  n_iter = 4,
+                  n_iter = bayesLen,
                   acq = "ucb",
-                  kappa = 4.0,
+                  kappa = 2.9,
                   eps = 0.0,
                   verbose = TRUE
 )
-
-
-save.image(file="test.RData")
+save.image(file="post_rand_search_1.RData")
 
 
 
